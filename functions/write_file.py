8c5f404d8a42865e7135ba4ba1b0354a45eb96ad
Jerrod Mathis <jerrodmathis@proton.me>
2025-10-03 12:31:58 -0400

    add helper to write to file with content

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_wd = os.path.abspath(working_directory)
+        rel_path = os.path.join(working_directory, file_path)
+        abs_path = os.path.abspath(rel_path)
+        if not abs_path.startswith(abs_wd):
+            raise Exception(f'Cannot write to "{
+                            file_path}" as it is outside the permitted working directory')
+    except OSError as oserr:
+        print(f'Error: {oserr}')
+    except Exception as err:
+        print(f'Error: {err}')
+    else:
+        try:
+            if not os.path.exists(os.path.dirname(abs_path)):
+                os.makedirs(os.path.dirname(abs_path))
+            with open(abs_path, 'w') as f:
+                f.write(content)
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        except OSError as oserr:
+            print(f'Error: {oserr}')
