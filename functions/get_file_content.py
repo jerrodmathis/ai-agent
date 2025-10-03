56a96361d6e9ffa27364d2537696d0b78fd15e81
Jerrod Mathis <jerrodmathis@proton.me>
2025-10-03 11:42:28 -0400

    add helper to get file content, update to catch os errors

---
+++ functions/get_file_content.py
@@ -0,0 +1,31 @@
+import os
+
+MAX_CHARS = 10000
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        abs_wd = os.path.abspath(working_directory)
+        rel_path = os.path.join(working_directory, file_path)
+        abs_path = os.path.abspath(rel_path)
+        if not abs_path.startswith(abs_wd):
+            raise Exception(
+                f'Cannot read "{file_path}" as it is outside the permitted working directory')
+        if not os.path.isfile(abs_path):
+            raise Exception(
+                f'File not found or is not a regular file: "{file_path}"')
+    except OSError as oserr:
+        print(f'Error: {oserr}')
+    except Exception as err:
+        print(f'Error: {err}')
+    else:
+        try:
+            f = open(abs_path, 'r')
+        except OSError as oserr:
+            print(f'Error: {oserr}')
+        else:
+            content = f.read(MAX_CHARS)
+            if len(content) == MAX_CHARS:
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters].'
+            f.close()
+            return content
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,19 +2,29 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    abs_wd = os.path.abspath(working_directory)
-    rel_path = os.path.join(working_directory, directory)
-    abs_path = os.path.abspath(rel_path)
-    if not abs_path.startswith(abs_wd):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(abs_path):
-        return f'Error: "{directory}" is not a directory'
-
-    contents = os.listdir(abs_path)
-    file_info = []
-    for file in contents:
-        file_size = os.path.getsize(os.path.join(abs_path, file))
-        is_dir = os.path.isdir(os.path.join(abs_path, file))
-        file_info.append(f'- {file}: file_size={
-                         file_size} bytes, is_dir={is_dir}')
-    return '\n'.join(file_info)
+    try:
+        abs_wd = os.path.abspath(working_directory)
+        rel_path = os.path.join(working_directory, directory)
+        abs_path = os.path.abspath(rel_path)
+        if not abs_path.startswith(abs_wd):
+            raise Exception(
+                f'Cannot list "{directory}" as it is outside the permitted working directory')
+        if not os.path.isdir(abs_path):
+            raise Exception(f'"{directory}" is not a directory')
+    except OSError as oserr:
+        print(f'Error: {oserr}')
+    except Exception as err:
+        print(f'Error: {err}')
+    else:
+        try:
+            file_info = list(map(lambda f: (
+                f,
+                os.path.getsize(os.path.join(abs_path, f)),
+                os.path.isdir(os.path.join(abs_path, f))
+            ), os.listdir(abs_path)))
+        except OSError as oserr:
+            print(f'Error: {oserr}')
+        else:
+            file_info_strings = list(map(
+                lambda info: f'- {info[0]}: file_size={info[1]}, is_dir={info[2]}', file_info))
+            return '\n'.join(file_info_strings)
