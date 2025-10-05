44bb1111734bcbb2c80b0ce4e08e12ae7c6b54dd
Jerrod Mathis <jerrodmathis@proton.me>
2025-10-04 20:55:44 -0400

    add method to call functions

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 
 system_prompt = """
 You are a helpful AI coding agent.
@@ -34,6 +34,45 @@ available_functions = types.Tool(
     ]
 )
 
+function_name_map = {
+    'get_files_info': get_files_info,
+    'get_file_content': get_file_content,
+    'run_python_file': run_python_file,
+    'write_file': write_file,
+}
+
+
+def call_function(function_call_part: types.FunctionCall, verbose=False):
+    name = function_call_part.name
+    args = function_call_part.args
+    if verbose:
+        print(f"Calling function: {name}({args})")
+    else:
+        print(f"Calling function: {name}")
+
+    if name in function_name_map:
+        result = function_name_map[name](
+            working_directory='./calculator', **args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=name,
+                    response={"result": result},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=name,
+                    response={"error": f"Unknown function: {name}"},
+                )
+            ],
+        )
+
 
 def main():
     if len(sys.argv) <= 1:
@@ -41,7 +80,9 @@ def main():
         print("Usage: python3 main.py <prompt>")
         sys.exit(1)
 
-    prompt = sys.argv[1]
+    prompt, *optionalArgs = sys.argv[1:]
+    verbose = "--verbose" in optionalArgs
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
@@ -58,13 +99,17 @@ def main():
 
     if response.function_calls is not None:
         for function_call in response.function_calls:
-            print(f'Calling function: {
-                  function_call.name}({function_call.args})')
+            result = call_function(function_call, verbose)
+            if result.parts[0].function_response.response:
+                if verbose:
+                    print(
+                        f"-> {result.parts[0].function_response.response}")
+            else:
+                raise Exception('Fatal error: no response from function call')
     else:
         print(response.text)
 
-    optionalArgs = sys.argv[2:]
-    if ("--verbose" in optionalArgs):
+    if (verbose):
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
